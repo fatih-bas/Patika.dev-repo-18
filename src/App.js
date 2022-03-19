81ba8e5d26228a955904fc3709c7041a03e0f07f
fatih-bas <fatihbas1@gmail.com>
2022-03-19 16:06:31 +0300

    second commit

--- src/App.js
+++ src/App.js
@@ -1,15 +1,14 @@
+import React from "react";
 import WeatherProvider from "./context/WeatherContext";
-import DropDown from './components/DropDown';
-import Cards from './components/Cards';
+import DropDown from "./components/DropDown";
+import Cards from "./components/Cards";
 
 function App() {
   return (
-    
-      <WeatherProvider>
-        <Cards />
-        <DropDown />
-      </WeatherProvider>
-
+    <WeatherProvider>
+      <Cards />
+      <DropDown />
+    </WeatherProvider>
   );
 }
 
